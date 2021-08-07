94d8042867cb989e0e78ece0516d40797e790787
Welton Nogueira <88469166+NogueiraWelton@users.noreply.github.com>
2021-08-07 18:15:05 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const scrollSections = document.querySelectorAll(".scroll");
+const halfWindow = window.innerHeight * 0.5;
+
+function scrollAnimation() {
+  scrollSections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = sectionTop - halfWindow < 0;
+
+    if (isSectionVisible) {
+      section.classList.add("active");
+    } else {
+      section.classList.remove("active");
+    }
+  });
+}
+
+const openMenu = document.querySelector(".hamburguer-menu");
+
+function toggleMenu() {
+  const menu = document.querySelector(".header-nav-menu");
+  const links = document.querySelectorAll(".header-nav-menu li a");
+
+  menu.classList.toggle("active");
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("active");
+    });
+  });
+}
+
+window.onload = () => {
+  window.addEventListener("scroll", scrollAnimation);
+  openMenu.addEventListener("click", toggleMenu);
+};
